cb75cf5c5c0d5523db723ae85fd5bb7b43ef5c3c
123silvao <sc.silvio321@gmail.com>
2025-05-16 09:33:29 -0300

    criando usuarios

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ import express from 'express'
 
 const app = express ()
 app.use(express.json())
+
+const users = []
  
 app.get('/usuarios/', (req, res) => {
-    
 
-    res.send("ROTA ACESSADA COM SUCESSO")
+    res.status(200).json(users)
+    
 })
 
     app.post('/usuarios', (req, res) =>{
-        //users.push(req.body)
+        users.push(req.body)
 
-        //res.status(201).json({message:"Usuario criado com sucesso"})
-        res.send("OLA")
+        res.status(201).json({message:"Usuario criado com sucesso"})
+       
     })
 
     app.listen(3000)
